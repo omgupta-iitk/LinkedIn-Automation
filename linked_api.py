8a65e1146c733a6f44729b6a7523f25f2dd2acb9
Om Gupta <152871368+omgupta-iitk@users.noreply.github.com>
2025-02-21 02:54:12 +0530

    Update linked_api.py

--- linked_api.py
+++ linked_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def linkedin_post_agent(text, content, media=False):
-    key = 'AQWd4VvflIfikRxVgASOMbDNS3sz5eyIumQyYcrCKyYtRAxfwjV-mCtIlNAtRs748STigwvwbJXK6qe_gJWuru0zYYdXUQXiTO0C3gp9AJwYjr771d4bVs_R9ijI2nJ7jgwwL95fqPc0TgxN7z6jjwsb2511PaBOYYRl9GvWVPQk7I4DFfaDJIGBGvgcWGnQ8PDFLPnIqgOW_ICcLjMYwhU-HUScBHrLh5B-QCMTubh8PH0ImERngWiYUDo_jzH8GvCQGrmlEpZRobgz4Q9oVo7G0GItdq8NrFC_bKhLXemgtZ2CPoswNZcO59WnMo4FF5gOXnFclsbZDpJYnJTHVMrsUFLsMA'
+    key = 'KEY'
     get_header = {
         'Authorization': 'Bearer {key}'.format(key=key),
     }
